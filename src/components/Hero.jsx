6781f113788e23ef7da3d39d2652a1421788206d
DGebrie <81825713+DGebrie@users.noreply.github.com>
2022-10-19 13:47:03 -0700

    installations/ start project

---
+++ src/components/Hero.jsx
@@ -0,0 +1,15 @@
+import React from "react";
+import { FontAwesomeIcon } from "@fortawesome/react-fontawesome";
+import { MailIcon } from "@heroicons/react/outline";
+
+const Hero = () => {
+  return (
+    <div className="w-full h-screen bg-zinc-200 flex flex-col justify-between">
+      {/* <MailIcon />
+      <FontAwesomeIcon icon="fa-brands fa-linkedin" />
+      <FontAwesomeIcon icon="fa-brands fa-github" /> */}
+    </div>
+  );
+};
+
+export default Hero;
