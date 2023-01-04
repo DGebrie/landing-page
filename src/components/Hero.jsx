7c4e6dbbc20555be385dc2f818e0e0e10b4b7af5
DGebrie <81825713+DGebrie@users.noreply.github.com>
2023-01-04 14:14:02 -0700

    paragraph

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -26,16 +26,17 @@ const Hero = () => {
           </div>
         </div>
         <div className="p-8">
-          <p className="text-2xl font-bold pb-5 text-center">
-            Hi, I am a Software Engineer.
-          </p>
           <p>
-            I have experience blah blah blah i am the best I have experience
-            blah blah blah i am the bestI have experience blah blah blah i am
-            the bestI have experience blah blah blah i am the best I have
-            experience blah blah blah i am the bestI have experience blah blah
-            blah i am the best
+            As a child, playing games, watching videos, even communicating with
+            friends on social media sites, I've always wanted to learn what it
+            took to create these apps and sites. After spending years as a
+            banker, I decided to take a leap of faith and feed my inner child's
+            desire to build sites. After attending a bootcamp, spending
+            countless hours on Google, Youtube and other sites to expand my
+            knowledge in coding, I am proud to call myself a ...
+            <span className="text-2xl font-bold pb-5 ">Software Engineer.</span>
           </p>
+
           {/* Add Icons of languages  */}
         </div>
       </div>
