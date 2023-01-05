3ebae2b0e1feeeba3ee0af4335dce0303adb49cd
DGebrie <81825713+DGebrie@users.noreply.github.com>
2023-01-05 00:18:20 -0700

    resume link

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -16,7 +16,7 @@ const Hero = () => {
               </button>
             </a>
             <a
-              href="https://drive.google.com/file/d/1IMOeA509Gge9ZjxI4wzqUzihNEoDa2Zx/view?usp=sharing"
+              href="https://drive.google.com/file/d/1jPZbZay6c4a3SOak5_MLRmtys8s4uZZm/view?usp=sharing"
               target="_blank"
             >
               <button class="bg-pink-600 hover:bg-grey-700 text-white font-bold py-2 px-4 border border-gray-700 rounded ml-3">
