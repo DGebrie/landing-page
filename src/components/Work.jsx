c27b705e4359491f3b6dda520bbd5cfa8328ffe7
DGebrie <81825713+DGebrie@users.noreply.github.com>
2023-01-05 00:18:41 -0700

    url update project

--- src/components/Work.jsx
+++ src/components/Work.jsx
@@ -33,10 +33,7 @@ const Work = () => {
                 Budget Tool
               </span>
               <div className="pt-8 text-center">
-                <a
-                  href="https://piggybank-budgeting.netlify.app/"
-                  target="_blank"
-                >
+                <a href="https://pennypincher.netlify.app/" target="_blank">
                   <button className="text-center rounded-lg px-4 py-3 m-2 bg-white text-gray-700 font-bold text-lg">
                     Demo
                   </button>
